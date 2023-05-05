e28a8048dd23dd4d526b36c2486a208986c268a5
djmacalalad <davykalawmacalalad@gmail.com>
2023-05-05 11:37:27 +0800

    added routinga and few other changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { LoginModalComponent } from './login-modal/login-modal.component';
 import { SignupModalComponent } from './signup-modal/signup-modal.component';
-
+import { ButtonModule } from 'primeng/button'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+ 
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +17,8 @@ import { SignupModalComponent } from './signup-modal/signup-modal.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
+    ButtonModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
